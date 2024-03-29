5b9b1b639c846c792db9cbd133ef88afdb0ceb7c
pd21 <pd.priyanka.21@gmail.com>
2024-03-29 17:57:57 +0530

    added axe core

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react"
 import ImageComp from "./Image";
+import ReactDOM  from "react-dom"
+
+
+if(process.env.NODE_ENC !== "production") {
+    const axe = require("@axe-core/react");
+    axe(React, ReactDOM,1000); 
+}
 
 function App() {
   return (
